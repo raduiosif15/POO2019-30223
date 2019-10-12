9b5577157ab848b793b93ef4d2eef619801f2194
ChirasValentin <56472147+ChirasValentin@users.noreply.github.com>
2019-10-12 13:53:16 +0300

    Tema 1

---
+++ Students/Chiras Valentin Fanica/LimiteleReprezentarilor.java
@@ -0,0 +1,18 @@
+
+public class LimiteleReprezentarilor {
+		public static void main(String arg[]) {
+			
+		//byte ByteNumber=127 + 4; 
+		//short ShortNumber=32767 +1;
+		int Number=2147483647 + 6 ;
+		
+		byte Byte1Number=127 - 4; 
+		short Short1Number=32767 - 1;
+		int Number1=2147483647 - 6 ;
+		
+		float Numar_virgula=3.4e38f;
+		double Numar_virgula2=1.7e308;
+		//float diferenta=3.4e39f-3.4e36f;
+		
+		}
+}
