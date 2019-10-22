8cb4da15acc011181d09cb0e91e3bf53da2858f8
VladGoia <VladGoia@users.noreply.github.com>
2019-10-22 22:11:47 +0300

    Tema2-Cerinte laborator

---
+++ Students/Goia Vlad - Razvan/Tema2(lab)/6din49/src/Sanse.java
@@ -0,0 +1,13 @@
+
+public class Sanse {
+
+	public static void main(String[] args) {
+		double x = 1;
+		double z = 13983816;
+		
+		double sanse = x/z;
+		System.out.println("Sanse de castig: "+ sanse);
+
+	}
+
+}
