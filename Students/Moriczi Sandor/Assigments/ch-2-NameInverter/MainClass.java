2187a41644c45aee6279dfc890aa9aba623a1559
moriczisanyi <smoriczi@yahoo.com>
2019-10-28 21:37:08 +0200

    Aici sunt temele din saptamanile anterioare pe care nu le-am trimis la timp.

---
+++ Students/Moriczi Sandor/Assigments/ch-2-NameInverter/MainClass.java
@@ -0,0 +1,11 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		NameInverter inv= new NameInverter("Mr. Phd. Moriczi Sanyi");
+		String a=inv.schimba();
+		System.out.println(a);
+
+	}
+
+}
