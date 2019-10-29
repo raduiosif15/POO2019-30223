8830c52e4ff81202ce6de35445b751cb9739b77b
Madalina <madaluna69@gmail.com>
2019-10-30 01:59:04 +0200

    Laboratorul 4 :)

---
+++ Students/Jula Madalina - Veronica/lab4/src/lab4/Sofer.java
@@ -0,0 +1,22 @@
+package lab4;
+
+public class Sofer {
+
+	private String nume;
+	private String prenume;
+	private int varsta;
+	private int idPermis;
+	
+	Sofer(){
+		
+	}
+	
+	Sofer(String nume, String prenume, int varsta, int idPermis){
+		this.nume=nume;
+		this.prenume=prenume;
+		this.varsta=varsta;
+		this.idPermis=idPermis;
+	}
+	
+	
+}
---
+++ Students/Jula Madalina - Veronica/restaurant/src/restaurant/Mese.java
@@ -0,0 +1,22 @@
+package restaurant;
+
+public class Mese {
+
+	private int idMasa;
+	private Boolean stare;
+	private int nrClienti; 
+	private float nota;
+	
+	Mese(){
+		
+	}
+	
+	Mese(int idMAsa, Boolean stare, int nrCLienti, float nota){
+		this.idMasa=idMasa;
+		this.stare=stare;
+		this.nrClienti=nrClienti;
+		this.nota=nota;
+	}
+	
+	
+}
