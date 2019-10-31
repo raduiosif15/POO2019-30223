b21559612094ab441d9bd232d4ebcf320f00cee4
Georgiu Alin <alingeorgiu@yahoo.com>
2019-10-31 16:34:11 +0200

    late late

---
+++ Students/Georgiu Alin - Ionel/Laboratoare/Lab4/Autovehicul/src/Sofer.java
@@ -0,0 +1,13 @@
+
+public class Sofer {
+	public String nume;
+	public String prenume;
+	public int varsta;
+	public int nrPermis;
+	public Sofer(String nume,String prenume,int varsta,int nrPermis) {
+		this.nume=nume;
+		this.prenume=prenume;
+		this.varsta=varsta;
+		this.nrPermis=nrPermis;
+	}
+}
