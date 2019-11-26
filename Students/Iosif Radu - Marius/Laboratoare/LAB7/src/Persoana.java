a53773987f8e06b3d6ef951062226444fd68cd79
Iosif Radu <raduiosif15@ahoo.com>
2019-11-27 00:08:13 +0200

    Lab-WIP

--- Students/Iosif Radu - Marius/Laboratoare/LAB7/src/Persoana.java
+++ Students/Iosif Radu - Marius/Laboratoare/LAB7/src/Persoana.java
@@ -1,7 +1,12 @@
-public class Persoana {
+public class Persoana implements Comparable {
     public String nume;
     public String prenume;
     public int varsta;
+
+    public Persoana(String nume, String prenume) {
+        this.nume = nume;
+        this.prenume = prenume;
+    }
 
     public void setNume(String nume){
         this.nume = nume;
@@ -25,5 +30,20 @@ public class Persoana {
 
     public int getVarsta(){
         return this.varsta;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Persoana persoanaAux = (Persoana) o;
+        if (this.nume.compareTo(persoanaAux.nume) == 0) {
+            return this.prenume.compareTo(persoanaAux.prenume);
+        }
+
+        return this.nume.compareTo(persoanaAux.nume);
+    }
+
+    @Override
+    public String toString() {
+        return this.nume + " " + this.prenume;
     }
 }
--- Students/Iosif Radu - Marius/Laboratoare/LAB7/src/TestPersoana.java
+++ Students/Iosif Radu - Marius/Laboratoare/LAB7/src/TestPersoana.java
@@ -1,22 +1,29 @@
 import java.lang.reflect.Array;
 import java.net.SocketOption;
+import java.util.Arrays;
 
 public class TestPersoana {
     public static void main(String[] args) {
-        Persoana[] persoane = new Persoana[3];
+        Persoana[] persoane = new Persoana[4];
 
-        persoane[0].setNume("Iosif");
-        persoane[1].setNume("Crecan");
-        persoane[2].setNume("State");
+        Persoana persoana0 = new Persoana("Iosif", "Radu");
+        Persoana persoana1 = new Persoana("Crecan", "Dragos");
+        Persoana persoana3 = new Persoana("Crecan", "Aragos");
+        Persoana persoana2 = new Persoana("State", "Marin");
 
-        persoane[0].setPrenume("Radu");
-        persoane[1].setPrenume("Dragos");
-        persoane[2].setPrenume("Marin");
+        persoane[0] = persoana0;
+        persoane[1] = persoana1;
+        persoane[2] = persoana2;
+        persoane[3] = persoana3;
 
         persoane[0].setVarsta(19);
         persoane[1].setVarsta(20);
         persoane[2].setVarsta(21);
 
-        
+        Arrays.sort(persoane);
+
+        for (int i = 0; i < persoane.length; i++) {
+            System.out.println(persoane[i].toString());
+        }
     }
 }
